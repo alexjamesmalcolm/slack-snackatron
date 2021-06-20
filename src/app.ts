8cf7b9ce1f9de99810c2ec127b5f5a910ad920cb
Alexander James Malcolm <alexjamesmalcolm@gmail.com>
2021-06-20 00:33:27 -0400

    Added root route

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,28 @@
-import { App } from "@slack/bolt";
+import { App, ExpressReceiver } from "@slack/bolt";
+
 const token = process.env.SLACK_TOKEN;
 const signingSecret = process.env.SLACK_SIGNING_SECRET;
+if (!token) {
+  throw new Error("SLACK_TOKEN is not defined");
+}
+if (!signingSecret) {
+  throw new Error("SLACK_SIGNING_SECRET is not defined");
+}
+
+const receiver = new ExpressReceiver({ signingSecret });
+
 const app = new App({
   token,
-  signingSecret,
+  receiver,
 });
 
 app.message("snackatron", async ({ message, say }) => {
   console.log(JSON.stringify(message));
   await say(`Hello there!`);
+});
+
+receiver.router.get("/", (req, res) => {
+  res.send("Hello World!");
 });
 
 const port = (process.env.PORT && Number.parseInt(process.env.PORT)) || 3000;
