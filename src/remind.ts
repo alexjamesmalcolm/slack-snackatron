effc6e56180a82cfbb00a37e5634a7244a2aefe3
Alexander James Malcolm <alexjamesmalcolm@gmail.com>
2021-06-19 23:41:41 -0400

    Commented out remind.ts and moved it to src

---
+++ src/remind.ts
@@ -0,0 +1,14 @@
+// import { scheduleJob, RecurrenceRule } from "node-schedule";
+// import { notify } from "node-notifier";
+// import { connect } from "../mongodb";
+
+// export const scheduleReminder = () => {
+//   const rule: RecurrenceRule = new RecurrenceRule();
+//   rule.minute = 0;
+//   scheduleJob(rule, async () => {
+//     const [mongo, close] = await connect();
+//     console.log("Hello");
+//     notify("Hello");
+//     close();
+//   });
+// };
